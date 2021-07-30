6e9ff308620012692bf505cc6657350e47dc6f3b
James Li <jamespeilunli@gmail.com>
2021-07-30 10:25:59 -0700

    let tetromino be moved multiple times every y level

--- tetris.rb
+++ tetris.rb
@@ -8,26 +8,33 @@ game = Game.new
 
 size = 50
 
-set title: "Tetrominos"
+set title: "Tetris"
 set width: size*game.gameboard.width
 set height: size*game.gameboard.height
 
+
+
 t = 1
 update do
-    if t % 30 == 0
-        game.draw([0, 0], size)
-        game.tetromino.fall
-        game.gameboard = game.tetromino.gameboard
-        game.tetromino.moved = false
-    end
+  if t % 8 == 0
+    game.tetromino.moved = false
+  end
 
-    on :key_down do |event|
-        if !game.tetromino.moved
-            game.tetromino.moved = game.tetromino.move(event.key)
-        end
-    end
+  if t % 30 == 0
+    game.draw([0, 0], size)
+    game.tetromino.fall
+    game.gameboard = game.tetromino.gameboard
+  end
 
-    t += 1
+  t += 1
+end
+
+on :key_held do |event|
+  if !game.tetromino.moved
+    game.tetromino.moved = game.tetromino.move(event.key)
+    game.gameboard = game.tetromino.gameboard
+    game.draw([0, 0], size)
+  end
 end
 
 show
--- tetrominos.rb
+++ tetrominos.rb
@@ -1,64 +1,59 @@
 require "matrix" # matrix is installed when you install ruby, no need to use gem. docs: https://ruby-doc.org/stdlib-2.5.1/libdoc/matrix/rdoc/Matrix.html
 
 class Tetromino # A tetromino is a tetris piece
-    attr_reader :piece_data, :color_map, :width, :height, :gameboard
-    attr_accessor :pos, :moved
+  attr_reader :piece_data, :color_map, :width, :height, :gameboard
+  attr_accessor :pos, :moved
 
-    def initialize(gameboard, piece_data, pos)
-        raise ArgumentError unless piece_data.is_a? Matrix
-        
-        @gameboard = gameboard
-        @pos = pos
-        @piece_data = piece_data
-        @width = piece_data.row(0).to_a.length
-        @height = piece_data.column(0).to_a.length
-        @moved = false
-    end
+  def initialize(gameboard, piece_data, pos)
+    raise ArgumentError unless piece_data.is_a? Matrix
+    
+    @gameboard = gameboard
+    @pos = pos
+    @piece_data = piece_data
+    @width = piece_data.row(0).to_a.length
+    @height = piece_data.column(0).to_a.length
+    @moved = false
+  end
 
-    def put_tetromino(clear=false)
-        (0...width).each do |i|
-            (0...height).each do |j|
-                if clear
-                    gameboard[pos[0]+j, pos[1]+i] = 0
-                else
-                    gameboard[pos[0]+j, pos[1]+i] = piece_data[j, i]
-                end
-            end
-        end
-    end
-
-    def fall
-        if pos[0] + height != gameboard.height
-            put_tetromino(clear=true)
-            pos[0] += 1
-            put_tetromino
-        end
-    end
-
-    def move(dir)
-        case dir
-        when "left"
-            if pos[1] > 0
-                put_tetromino(clear=true)
-                pos[1] -= 1
-                put_tetromino
-                return true
-            end
-            return false
-        when "right"
-            if pos[1] < gameboard.width - width
-                put_tetromino(clear=true)
-                pos[1] += 1
-                put_tetromino
-                return true
-            end
-            return false
-        when "down"
-            # adding acelleration later
-            return false
+  def put_tetromino(clear=false)
+    (0...width).each do |i|
+      (0...height).each do |j|
+        if clear
+          gameboard[pos[0]+j, pos[1]+i] = 0
         else
-            return false
+          gameboard[pos[0]+j, pos[1]+i] = piece_data[j, i]
         end
-        false
+      end
     end
+  end
+
+  def update(pos_index, inc)
+    put_tetromino(clear=true)
+    pos[pos_index] += inc
+    put_tetromino
+  end
+
+  def fall
+    if pos[0] + height != gameboard.height
+      update(0, 1)
+    end
+  end
+
+  def move(dir)
+    case dir
+    when "left"
+      if pos[1] > 0
+        update(1, -1)
+        return true
+      end
+    when "right"
+      if pos[1] < gameboard.width - width
+        update(1, 1)
+        return true
+      end
+    else
+      return false
+    end
+    false
+  end
 end
