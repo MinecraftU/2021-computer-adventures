eb7d614593a7b80cd3bd4ac1e0cca248ed192b91
James Li <jamespeilunli@gmail.com>
2021-07-30 15:10:18 -0700

    fix indentation

--- tetris.rb
+++ tetris.rb
@@ -13,28 +13,28 @@ set height: size*game.gameboard.height
 
 t = 1
 update do
-    if t % 12 == 0
-        game.tetromino.moved = false
-    end
+  if t % 12 == 0
+    game.tetromino.moved = false
+  end
 
-    if t % 30 == 0
-        if game.tetromino.is_dead
-            game.tetromino = Tetromino.new(game.gameboard, game.tetromino_shapes.sample, [0, 0])
-        end
-        game.draw([0, 0], size)
-        game.tetromino.fall
-        game.gameboard = game.tetromino.gameboard
+  if t % 30 == 0
+    if game.tetromino.is_dead
+      game.tetromino = Tetromino.new(game.gameboard, game.tetromino_shapes.sample, [0, 0])
     end
+    game.draw([0, 0], size)
+    game.tetromino.fall
+    game.gameboard = game.tetromino.gameboard
+  end
 
-    t += 1
+  t += 1
 end
 
 on :key_held do |event|
-    if !game.tetromino.moved
-        game.tetromino.moved = game.tetromino.move(event.key)
-        game.gameboard = game.tetromino.gameboard
-        game.draw([0, 0], size)
-    end
+  if !game.tetromino.moved
+    game.tetromino.moved = game.tetromino.move(event.key)
+    game.gameboard = game.tetromino.gameboard
+    game.draw([0, 0], size)
+  end
 end
 
 show
